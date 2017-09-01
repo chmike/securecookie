56a067f7c4714cdad6fdb8efcc99b5ef44d9e45c
Christophe Meessen <meessen@cppm.in2p3.fr>
2017-09-01 15:54:36 +0200

    test validate encoding/decoding

--- cookie2.go
+++ cookie2.go
@@ -8,6 +8,7 @@ import (
 	"crypto/rand"
 	"errors"
 	"io"
+	"sync"
 )
 
 // MACLen is the byte length of the MAC.
@@ -35,155 +36,199 @@ func EncodedValLen(valLen int) int {
 	return ((l-l%3)*8 + 5) / 6
 }
 
+// GenerateRandomKey return a random key of KeyLen bytes.
+// Use hex.EncodeToString(key) to get the key as hexadecimal string,
+// and hex.DecodeString(keyStr) to convert back from string to byte slice.
+func GenerateRandomKey() ([]byte, error) {
+	key := make([]byte, KeyLen)
+	if _, err := io.ReadFull(rand.Reader, key); err != nil {
+		return nil, err
+	}
+	return key, nil
+}
+
 // KeyLen is the byte length of the key.
 const KeyLen = 32
 
-// EncodeValue encrypt and encode in base64 the content of val in place.
-// Grows val when cap(val) < encodedValLen(len(val)).
+// AppendEncodedValue encrypt and encode val in base64 and append it to out.
+// Grows out if cap(out) - len(out) < encodedValLen(len(val)).
 // Key is a random sequence of KeyLen bytes. Don't use a string.
-func EncodeValue(val []byte, key []byte) ([]byte, error) {
-	// grow val capacity if required
+func AppendEncodedValue(out, val []byte, key []byte) ([]byte, error) {
+	if val == nil {
+		val = make([]byte, 0)
+	}
+	// grow out capacity if required
 	maxRawLen := len(val) + 5 + MACLen
 	encLen := ((maxRawLen-maxRawLen%3)*8 + 5) / 6
-	if cap(val) < encLen {
-		tmp := make([]byte, len(val), encLen)
-		copy(tmp, val)
-		val = tmp
+	if cap(out)-len(out) < encLen {
+		tmp := make([]byte, len(out), encLen)
+		copy(tmp, out)
+		out = tmp
 	}
+	// append val to out
+	valPos := len(out)
+	out = out[:valPos+len(val)]
+	copy(out[valPos:], val)
 	// append random bytes
 	nRnd := 5 - maxRawLen%3
-	val = val[:len(val)+nRnd]
-	if _, err := io.ReadFull(rand.Reader, val[len(val)-nRnd:]); err != nil {
+	rndPos := len(out)
+	out = out[:rndPos+nRnd]
+	if _, err := io.ReadFull(rand.Reader, out[rndPos:]); err != nil {
 		return nil, err
 	}
-	val[len(val)-1] = (val[len(val)-1] & 0xFC) | byte(nRnd)%3
+	out[len(out)-1] = (out[len(out)-1] & 0xFC) | byte(nRnd)%3
 	// compute and append mac
-	mac := hmac.New(md5.New, key[:hmacKeyLen])
-	if _, err := mac.Write(val); err != nil {
+	mac := hmac.New(md5.New, key[:MACLen])
+	if _, err := mac.Write(out[valPos:]); err != nil {
 		return nil, err
 	}
-	ivPos := len(val)
-	mac.Sum(val)
+	ivPos := len(out)
+	out = mac.Sum(out)
+	iv := out[ivPos:]
 	// encrypt value, random bytes and mac
-	iv := val[ivPos:]
-	block, err := aes.NewCipher(key[hmacKeyLen:])
+	block, err := aes.NewCipher(key[MACLen:])
 	if err != nil {
 		return nil, err
 	}
 	block.Encrypt(iv, iv) // encrypt mac to get iv
 	stream := cipher.NewCTR(block, iv)
-	stream.XORKeyStream(val[:ivPos], val[:ivPos])
+	stream.XORKeyStream(out[valPos:ivPos], out[valPos:ivPos])
 	// encode in base64
-	return encodeBase64(val), nil
+	return appendEncodedBase64(out[:valPos], out[valPos:]), nil
 }
 
-// DecodeValue decode base64 and decrypt value in place.
-// Return the slice of value bytes.
+var bufPool = sync.Pool{New: func() interface{} { b := make([]byte, 64); return &b }}
+
+// AppendDecodedValue decode base64 and decrypt value in place.
+// Return the slice of decoded value.
 // Key is a random sequence of KeyLen bytes. Don't use a string.
-func DecodeValue(val []byte, key []byte) ([]byte, error) {
-	if len(val)%4 != 0 {
-		return errors.New("invalid encoded value length")
+func AppendDecodedValue(out, val []byte, key []byte) ([]byte, error) {
+	var err error
+	if len(val) < 21 || len(val)%4 != 0 {
+		return nil, errors.New("invalid encoded value length")
 	}
-	// decode base64
-	val, err := decodeBase64(val)
-	if err != nil {
+	// decode base64 encoded val into a temporary buffer
+	bPtr := bufPool.Get().(*[]byte)
+	defer bufPool.Put(bPtr)
+	b := (*bPtr)[:0]
+	if b, err = appendDecodedBase64(b, val); err != nil {
 		return nil, err
 	}
+	bPtr = &b
 	// decrypt value
-	ivPos := len(val) - MACLen
-	iv := val[ivPos:]
-	val = val[:ivPos]
-	block, err := aes.NewCipher(key[hmacKeyLen:])
+	ivPos := len(b) - MACLen
+	iv := b[ivPos:]
+	b = b[:ivPos]
+	block, err := aes.NewCipher(key[MACLen:])
 	if err != nil {
 		return nil, err
 	}
 	stream := cipher.NewCTR(block, iv)
-	stream.XORKeyStream(val, val)
+	stream.XORKeyStream(b, b)
 	block.Decrypt(iv, iv) // decrypt iv to get mac
 	// compute and check mac
-	mac := hmac.New(md5.New, key[:hmacKeyLen])
-	if _, err := mac.Write(val); err != nil {
+	mac := hmac.New(md5.New, key[:MACLen])
+	if _, err := mac.Write(b); err != nil {
 		return nil, err
 	}
 	if !hmac.Equal(iv, mac.Sum(nil)) {
 		return nil, errors.New("MAC mismatch")
 	}
 	// drop random bytes and return value
-	nRnd := int(3 + val[len(val)-1]&0x3)
+	nRnd := int(3 + b[len(b)-1]&0x3)
 	if nRnd == 6 {
 		return nil, errors.New("invalid number of random bytes")
 	}
-	return val[:len(val)-nRnd], nil
+	b = b[:len(b)-nRnd]
+	// append decoded value to out
+	return append(out, b...), nil
 }
 
-// encodeBase64 encodes val in place into base64.
+// encodeBase64 encodes val in place into base64. src and dst may overlap.
 // Requires: val != nil && cap(val) >= (len(val)*8+5)/6 && len(val)%3 == 0.
-func encodeBase64(val []byte) []byte {
-	src := len(val)
-	dst := (src*8 + 5) / 6
-	val = val[:dst]
-	for src > 0 {
-		v := uint64(val[src-1]) | uint64(val[src-2])<<8 | uint64(val[src-3])<<16
-		val[dst-1] = base64Char(byte(v))
-		val[dst-2] = base64Char(byte(v >> 6))
-		val[dst-3] = base64Char(byte(v >> 12))
-		val[dst-4] = base64Char(byte(v >> 18))
-		src -= 3
-		dst -= 4
+func appendEncodedBase64(dst, src []byte) []byte {
+	encLen := (len(src)*8 + 5) / 6
+	if cap(dst)-len(dst) < encLen {
+		tmp := make([]byte, len(dst), len(dst)+encLen)
+		copy(tmp, dst)
+		dst = tmp
 	}
-	return val
+	srcIdx := len(src)
+	dstIdx := len(dst) + encLen
+	dst = dst[:dstIdx]
+	for srcIdx > 0 {
+		srcIdx--
+		v := uint64(src[srcIdx])
+		srcIdx--
+		v |= uint64(src[srcIdx]) << 8
+		srcIdx--
+		v |= uint64(src[srcIdx]) << 16
+		dstIdx--
+		dst[dstIdx] = base64Char(byte(v))
+		dstIdx--
+		dst[dstIdx] = base64Char(byte(v >> 6))
+		dstIdx--
+		dst[dstIdx] = base64Char(byte(v >> 12))
+		dstIdx--
+		dst[dstIdx] = base64Char(byte(v >> 18))
+	}
+	return dst
 }
 
-// base64Byte2Char converts a byte to Base64 URL encoding.
+// base64Char converts a byte to Base64 URL encoding.
 // The two most significant bits are ignored.
 // See https://tools.ietf.org/html/rfc4648#section-5.
 func base64Char(b byte) byte {
 	b &= 0x3F
-	switch {
-	case b < 26:
+	if b < 26 {
 		return b + 'A'
-	case b < 52:
+	} else if b < 52 {
 		return b + 'a' - 26
-	case b < 62:
+	} else if b < 62 {
 		return b + '0' - 52
-	case b == 62:
+	} else if b == 62 {
 		return '-'
-	case b == 63:
-		return '_'
 	}
-	return 0 // never reached
+	return '_' // b == 63
 }
 
-// decodeBase64 decode base64 encoded val in place.
-// requires len(val)%4 == 0.
-func decodeBase64(val []byte) ([]byte, error) {
-	var src, dst int
-	for dst < len(val) {
+// appendDecodedBase64 decode src into dst. src and dst may be the same buffer.
+// Requires len(src)%4 == 0 && cap(dst) - len(dst) >= (len(src)*6)/8.
+func appendDecodedBase64(dst, src []byte) ([]byte, error) {
+	decLen := (len(src) * 6) / 8
+	if cap(dst)-len(dst) < decLen {
+		tmp := make([]byte, len(dst), len(dst)+decLen)
+		copy(tmp, dst)
+		dst = tmp
+	}
+	var srcIdx int
+	dstIdx := len(dst)
+	dst = dst[:dstIdx+decLen]
+	for srcIdx < len(src) {
 		var v uint64
 		for i := 0; i < 4; i++ {
-			b := val[src]
-			src++
-			switch {
-			case b >= 'A' && b <= 'Z':
+			b := src[srcIdx]
+			if b >= 'A' && b <= 'Z' {
 				v = (v << 6) | uint64(b-'A')
-			case b >= 'a' && b <= 'z':
+			} else if b >= 'a' && b <= 'z' {
 				v = (v << 6) | uint64(b-'a'+26)
-			case b >= '0' && b <= '9':
+			} else if b >= '0' && b <= '9' {
 				v = (v << 6) | uint64(b-'0'+52)
-			case b == '-':
+			} else if b == '-' {
 				v = (v << 6) | uint64(62)
-			case b == '_':
+			} else if b == '_' {
 				v = (v << 6) | uint64(63)
-			default:
+			} else {
 				return nil, errors.New("invalid base64 char")
 			}
+			srcIdx++
 		}
-		val[dst] = byte(v >> 16)
-		dst++
-		val[dst] = byte(v >> 8)
-		dst++
-		val[dst] = byte(v)
-		dst++
+		dst[dstIdx] = byte(v >> 16)
+		dstIdx++
+		dst[dstIdx] = byte(v >> 8)
+		dstIdx++
+		dst[dstIdx] = byte(v)
+		dstIdx++
 	}
-	return val[:src], nil
+	return dst, nil
 }
--- cookie2_test.go
+++ cookie2_test.go
@@ -1,21 +1,115 @@
 package cookie
 
-import "testing"
-import "bytes"
+import (
+	"bytes"
+	"testing"
+)
 
-func TestEncodeBase64(t *testing.T) {
+func TestAppendEncodedBase64(t *testing.T) {
 	tests := []struct {
 		in, out []byte
 	}{ // requires that len(in)%3 == 0
 		{in: []byte{}, out: []byte{}},
 		{in: []byte{0, 0, 0}, out: []byte{65, 65, 65, 65}},
-		{in: []byte{FF, FF, FF}, out: []byte{65, 65, 65, 65}},
-	}
-
+		{in: []byte{255, 255, 255}, out: []byte{95, 95, 95, 95}},
+		{in: []byte{0, 1, 2, 3, 4, 5}, out: []byte{65, 65, 69, 67, 65, 119, 81, 70}},
+		{in: []byte{3, 200, 254}, out: []byte{65, 56, 106, 45}},
+	} // out == base64.URLEncoding.WithPadding(base64.NoPadding).Encode(out, in)
 	for _, test := range tests {
-		out := string(encodeBase64(test.in))
+		out := appendEncodedBase64(nil, test.in)
 		if !bytes.Equal(out, test.out) {
 			t.Errorf("got output %v, expected %v for input %v", out, test.out, test.in)
 		}
 	}
+}
+
+func TestAppendDecodedBase64(t *testing.T) {
+	tests := []struct {
+		in, out []byte
+		fail    bool
+	}{ // requires that len(in)%4 == 0
+		{in: []byte{}, out: []byte{}},
+		{in: []byte("AAAA"), out: []byte{0, 0, 0}},
+		{in: []byte("____"), out: []byte{255, 255, 255}},
+		{in: []byte("AAECAwQF"), out: []byte{0, 1, 2, 3, 4, 5}},
+		{in: []byte("AAEC AwQF"), out: nil, fail: true},
+		{in: []byte("A8j-"), out: []byte{3, 200, 254}},
+	} // out == base64.URLEncoding.WithPadding(base64.NoPadding).Encode(out, in)
+	for _, test := range tests {
+		out, err := appendDecodedBase64(nil, test.in)
+		if err != nil {
+			if !test.fail {
+				t.Errorf("got error '%s', expected no error", err)
+			}
+		} else if test.fail {
+			t.Errorf("got nil error, expected to fail")
+		}
+		if test.fail == (err != nil) && !bytes.Equal(out, test.out) {
+			t.Errorf("got output %v, expected %v for input %v", out, test.out, test.in)
+		}
+	}
+}
+
+func TestEncodedValLen(t *testing.T) {
+	tests := []struct {
+		in, out int
+	}{
+		{in: 0, out: 28},
+		{in: 1, out: 28},
+		{in: 2, out: 28},
+		{in: 3, out: 32},
+		{in: 4, out: 32},
+		{in: 5, out: 32},
+		{in: 6, out: 36},
+	}
+	for _, test := range tests {
+		out := EncodedValLen(test.in)
+		if out != test.out {
+			t.Errorf("got %d, expected %d for %d", out, test.out, test.in)
+		}
+	}
+}
+
+func TestEncodDecodeValue(t *testing.T) {
+	tests := []struct {
+		in               []byte
+		encFail, decFail bool
+	}{
+		{in: []byte{}},
+		{in: []byte{0, 0, 0}},
+		{in: []byte{255, 255, 255}},
+		{in: []byte{0, 1, 2, 3, 4, 5}},
+		{in: []byte{3, 200, 254}},
+	}
+	key, err := GenerateRandomKey()
+	if err != nil {
+		panic(err)
+	}
+	for _, test := range tests {
+		out, err := AppendEncodedValue(nil, test.in, key)
+		if (err == nil) == test.encFail {
+			if err == nil {
+				t.Errorf("got nil encoding error, expected error for in: %+v", test.in)
+			} else {
+				t.Errorf("got encoding error '%s', expected nil error for in: %+v", err, test.in)
+			}
+		} else {
+			in, err := AppendDecodedValue(nil, out, key)
+			if (err == nil) == test.decFail {
+				if err == nil {
+					t.Errorf("got nil decoding error, expected failure for in: %+v", test.in)
+				} else {
+					t.Errorf("got decoding error '%s', expected nil error for in: %+v", err, test.in)
+				}
+			} else {
+				if !bytes.Equal(in, test.in) {
+					t.Errorf("encoding->decoding mismatch")
+				}
+			}
+		}
+	}
+}
+
+func TestAppendDecodedValueErrors(t *testing.T) {
+
 }
